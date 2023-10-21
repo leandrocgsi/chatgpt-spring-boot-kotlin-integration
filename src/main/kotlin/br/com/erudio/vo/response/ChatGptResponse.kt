ad08e0ea27d7bf6714763d892a0c0a9d633d7a28
leandrocgsi <leandrocgsi@gmail.com>
2023-10-21 13:16:03 -0300

    Initial Commit

--- src/main/kotlin/br/com/erudio/vo/response/ChatGptResponse.kt
+++ src/main/kotlin/br/com/erudio/vo/response/ChatGptResponse.kt
@@ -4,10 +4,9 @@ data class ChatGptResponse (
 
     var choices: List<Choice?>? = mutableListOf()
 )
-/**
 {
     // Getter para a propriedade 'choices'
     fun getChoices(): List<Choice?>? {
         return choices
     }
-}*/
+}
