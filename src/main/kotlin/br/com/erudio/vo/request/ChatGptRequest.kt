b101749a3c62c42264252527d13bea5c909b7ba6
leandrocgsi <leandrocgsi@gmail.com>
2023-10-23 15:39:01 -0300

    Initial Commit

--- src/main/kotlin/br/com/erudio/vo/request/ChatGptRequest.kt
+++ src/main/kotlin/br/com/erudio/vo/request/ChatGptRequest.kt
@@ -2,8 +2,8 @@ package br.com.erudio.vo.request
 
 // data class ChatGptRequest(val prompt: String, val messages: List<Message>)
 
-data class ChatGptRequest(val model: String, val prompt: String) {
-    val messages: MutableList<Message> = mutableListOf(Message("user", prompt))
+data class ChatGptRequest(val model: String?, val prompt: String?) {
+    val messages: MutableList<Message> = mutableListOf(Message("user", prompt!!))
 }
 
 /*    init {
